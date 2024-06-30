aa9e3f82c453409f735a592e5c3c6476a2e2b391
Nischal Shetty <nischal2004coc@gmail.com>
2024-06-30 21:43:26 +0530

    moved to typescript

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <>Hello World!</>;
-}
-
-export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="flex-col text-center">
+          <div>EZOrder</div>
+          <div>Home Page</div>
+        </div>
+      </div>
+    </div>
+  );
+};
